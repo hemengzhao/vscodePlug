7b57081c1cd0ea5306e67768d24973e6a080b07e
hmz <18137930317@163.com>
2025-03-27 11:22:21 +0800

    fix: 所选文本全局替换替换

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,74 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "i18ntools" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "myi18ntools" is now active!');
+  const disposable = vscode.commands.registerCommand(
+    "i18ntools.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello World from i18nTools!");
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('myi18ntools.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from myI18nTools!');
-	});
+  const replaceDisposable = vscode.commands.registerCommand(
+    "i18ntools.replaceSelectedTextInProject",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showInformationMessage("没有活动的编辑器");
+        return;
+      }
 
-	context.subscriptions.push(disposable);
+      const selection = editor.selection;
+      const selectedText = editor.document.getText(selection);
+      if (!selectedText) {
+        vscode.window.showInformationMessage("没有选中任何文字");
+        return;
+      }
+
+      const newText = await vscode.window.showInputBox({
+        prompt: "请输入要替换成的文字",
+        placeHolder: "输入新的文字",
+      });
+
+      if (!newText) {
+        return;
+      }
+
+      const workspaceFolders = vscode.workspace.workspaceFolders;
+      if (!workspaceFolders) {
+        vscode.window.showInformationMessage("没有打开工作区");
+        return;
+      }
+
+      const rootPath = workspaceFolders[0].uri.fsPath;
+
+      const searchFiles = (dir: string) => {
+        const files = fs.readdirSync(dir);
+        for (const file of files) {
+          const filePath = path.join(dir, file);
+          const stat = fs.statSync(filePath);
+          if (stat.isDirectory()) {
+            searchFiles(filePath);
+          } else {
+            const content = fs.readFileSync(filePath, "utf8");
+            if (content.includes(selectedText)) {
+              const newContent = content.replace(
+                new RegExp(selectedText, "g"),
+                newText
+              );
+              fs.writeFileSync(filePath, newContent, "utf8");
+            }
+          }
+        }
+      };
+
+      searchFiles(rootPath);
+      vscode.window.showInformationMessage("替换完成");
+    }
+  );
+
+  context.subscriptions.push(disposable, replaceDisposable);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
